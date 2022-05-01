d4e51b28e1b72c337eb76b0e5f281d5052dd9d95
Fredrik Averpil <fredrik.averpil@gmail.com>
2022-05-01 15:56:20 +0200

    Fix various errors

---
+++ tests/crud_peewee/insert.sql
@@ -0,0 +1,1 @@
+INSERT INTO persons (FirstName, LastName) VALUES ('John', 'Doe');
--- tests/crud_peewee/test_create_table.py
+++ tests/crud_peewee/test_create_table.py
@@ -6,3 +6,14 @@ def test_peewee_create_table(peewee_db):
         peewee_db.execute_sql(
             pathlib.Path(__file__).parent.joinpath("create.sql").read_text()
         )
+
+    with peewee_db.atomic():
+        peewee_db.execute_sql(
+            pathlib.Path(__file__).parent.joinpath("insert.sql").read_text()
+        )
+
+    cursor = peewee_db.execute_sql("SELECT * FROM persons")
+    rows = cursor.fetchall()
+
+    assert len(list(rows)) == 1
+    assert rows[0] == (1, "John", "Doe", None, None)
