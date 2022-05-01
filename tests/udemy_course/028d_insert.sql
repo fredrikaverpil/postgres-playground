40a2e5d6280badb4ad3fd533d918d6dd30efc4af
Fredrik Averpil <fredrik.averpil@gmail.com>
2022-05-01 23:46:20 +0200

    Add udemy part 3

---
+++ tests/udemy_course/028d_insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO photos (url, user_id)
+VALUES ('http://one.jpg', 4),
+('http://two.jpg', 1),
+('http://25.jpg', 1),
+('http://36.jpg', 1),
+('http://754.jpg', 2),
+('http://35.jpg', 3),
+('http://256.jpg', 4);
