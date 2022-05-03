e7305ca4980996a8e9f0f932aa5961af957baaee
Fredrik Averpil <fredrik.averpil@gmail.com>
2022-05-03 08:32:38 +0200

    Rename userid

--- tests/exporter/test_exporter.py
+++ tests/exporter/test_exporter.py
@@ -14,13 +14,13 @@ def test_export_data(psycopg2_cursor, psycopg2_query):
     assert rows == [
         (
             [
-                {"url": "http://36.jpg", "user_id": 1, "username": "monahan93"},
-                {"url": "http://25.jpg", "user_id": 1, "username": "monahan93"},
-                {"url": "http://two.jpg", "user_id": 1, "username": "monahan93"},
-                {"url": "http://754.jpg", "user_id": 2, "username": "pferrer"},
-                {"url": "http://35.jpg", "user_id": 3, "username": "si93onis"},
-                {"url": "http://256.jpg", "user_id": 4, "username": "99stroman"},
-                {"url": "http://one.jpg", "user_id": 4, "username": "99stroman"},
+                {"url": "http://36.jpg", "username": "monahan93"},
+                {"url": "http://25.jpg", "username": "monahan93"},
+                {"url": "http://two.jpg", "username": "monahan93"},
+                {"url": "http://754.jpg", "username": "pferrer"},
+                {"url": "http://35.jpg", "username": "si93onis"},
+                {"url": "http://256.jpg", "username": "99stroman"},
+                {"url": "http://one.jpg", "username": "99stroman"},
             ],
         ),
     ]
