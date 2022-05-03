6aab2e4f97d51dcdd7a4697b8748b33edf9cda4a
Fredrik Averpil <fredrik.averpil@gmail.com>
2022-05-03 08:45:18 +0200

    Assert json from file is valid

--- tests/exporter/test_exporter.py
+++ tests/exporter/test_exporter.py
@@ -1,16 +1,23 @@
+import json
 import pathlib
 
 
-def test_export_json_data(psycopg2_cursor, psycopg2_query):
+def test_export_json_data(psycopg2_cursor, psycopg2_query, tmp_path):
     limit = 3
 
     psycopg2_cursor.execute(
         pathlib.Path(__file__).parent.joinpath("create_and_insert.sql").read_text(),
     )
 
-    sql = pathlib.Path(__file__).parent.joinpath("export.sql").read_text()
+    sql = pathlib.Path(__file__).parent.joinpath("export_json.sql").read_text()
     sql_params = (limit, limit)
     columns, rows = psycopg2_query(psycopg2_cursor, sql, sql_params)
+
+    json_filepath = tmp_path / "export.json"
+    with open(json_filepath, "w") as outfile:
+        json.dump(rows[0][0], outfile)
+    with open(json_filepath, "r") as infile:
+        loaded_json = json.load(infile)
 
     assert columns == ["json_agg"]
     assert rows == [
@@ -22,3 +29,4 @@ def test_export_json_data(psycopg2_cursor, psycopg2_query):
             ],
         )
     ]
+    assert rows[0][0] == loaded_json
