f3c93efc8b5da2e535ec344e33ed80efa6438804
Fredrik Averpil <fredrik.averpil@gmail.com>
2022-05-03 08:38:40 +0200

    Add variable limit

--- tests/exporter/test_exporter.py
+++ tests/exporter/test_exporter.py
@@ -2,13 +2,14 @@ import pathlib
 
 
 def test_export_data(psycopg2_cursor, psycopg2_query):
+    limit = 3
 
     psycopg2_cursor.execute(
         pathlib.Path(__file__).parent.joinpath("create_and_insert.sql").read_text(),
     )
 
     sql = pathlib.Path(__file__).parent.joinpath("export.sql").read_text()
-    sql_params = (3, 3)
+    sql_params = (limit, limit)
     columns, rows = psycopg2_query(psycopg2_cursor, sql, sql_params)
 
     assert columns == ["json_agg"]
