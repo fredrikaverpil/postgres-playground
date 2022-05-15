e3659a814bd71ba690a9a7d643e7624d4a214917
Fredrik Averpil <fredrik.averpil@gmail.com>
2022-05-15 10:10:29 +0200

    Add better cursor handling

--- tests/fixtures/orm/psycopg2.py
+++ tests/fixtures/orm/psycopg2.py
@@ -1,4 +1,5 @@
 import os
+from typing import Callable
 from urllib.parse import urlparse
 
 import psycopg2
@@ -11,7 +12,11 @@ DB = os.environ.get(
 
 @pytest.fixture()
 def psycopg2_cursor():
-    # ...
+    """Return psycopg2 cursor.
+
+    Note:
+        Use psycopg2_conn instead.
+    """
 
     uri = urlparse(DB)
 
@@ -47,10 +52,70 @@ def psycopg2_cursor():
 
 @pytest.fixture()
 def psycopg2_query():
-    def _psycopg2_query(cursor, sql, sql_params=None):
+    """Return rows, columns from a query.
+
+    Note:
+        Use psycopg2_execute instead.
+    """
+
+    def _(cursor, sql, sql_params=None):
         cursor.execute(sql, sql_params)
         columns = [field_md[0] for field_md in cursor.description]
         rows = cursor.fetchall()
         return columns, rows
 
-    return _psycopg2_query
+    return _
+
+
+@pytest.fixture()
+def psycopg2_conn():
+
+    uri = urlparse(DB)
+
+    username = uri.username
+    password = uri.password
+    database = uri.path[1:]
+    hostname = uri.hostname
+    port = uri.port
+
+    conn = psycopg2.connect(
+        user=username,
+        password=password,
+        host=hostname,
+        port=port,
+        dbname=database,
+    )
+
+    yield conn
+
+    drop_schema = """
+        DROP SCHEMA public CASCADE;
+        CREATE SCHEMA public;
+
+        GRANT ALL ON SCHEMA public TO postgres;
+        GRANT ALL ON SCHEMA public TO public;
+    """
+    cursor = conn.cursor()
+    cursor.execute(drop_schema)
+    cursor.close()
+    conn.close()
+
+
+@pytest.fixture()
+def psycopg2_execute(psycopg2_conn) -> Callable:
+    def _(sql, sql_params=None, commit=False):
+        cursor = psycopg2_conn.cursor()
+        cursor.execute(sql, sql_params)
+        if commit:
+            psycopg2_conn.commit()
+        return cursor
+
+    return _
+
+
+@pytest.fixture()
+def psycopg2_columns() -> Callable:
+    def _(cursor) -> list[str]:
+        return [field_md[0] for field_md in cursor.description]
+
+    return _
